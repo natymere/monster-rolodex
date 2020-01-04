82591e0da1a4701bf13d52f7363482c5a1ed0b09
jem <fearlessjem17@gmail.com>
2020-01-05 05:04:02 +0800

    Updates

---
+++ precache-manifest.100772d6522e10a50c0a637eaac83e9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd586a906615ff3e4c4a8927795e672",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5afafaeba585366976b6",
+    "url": "/monster-rolodex/static/css/main.a972c38c.chunk.css"
+  },
+  {
+    "revision": "b62b9052603d1f962399",
+    "url": "/monster-rolodex/static/js/2.382b7515.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.382b7515.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5afafaeba585366976b6",
+    "url": "/monster-rolodex/static/js/main.864d677f.chunk.js"
+  },
+  {
+    "revision": "99047a773d79ca6f96f0",
+    "url": "/monster-rolodex/static/js/runtime-main.ec5f649a.js"
+  }
+]);
